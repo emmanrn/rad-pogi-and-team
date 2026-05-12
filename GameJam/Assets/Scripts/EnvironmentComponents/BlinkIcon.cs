9c9b56e0faea0a8c9e06d2009a81cb056ecb0d0a
Arturo Andres de Silva <arturo.andres.desilva@gmail.com>
2026-05-12 23:30:41 +0800

    Slight UI Changes and Functionalities

---
+++ GameJam/Assets/Scripts/EnvironmentComponents/BlinkIcon.cs
@@ -0,0 +1,40 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BlinkIcon : MonoBehaviour
+{
+    [SerializeField] private Sprite[] sprites;
+    [SerializeField] private Image img;
+    [SerializeField] private TextMeshProUGUI txt;
+
+    private void Start()
+    {
+       DisableIconOnPlayerRoom();
+    }
+
+    private void DisableIconOnPlayerRoom()
+    {
+        if (SceneManager.GetActiveScene().name == RoomType.PlayRoom)
+        {
+            img.sprite = sprites[2];
+            txt.text = "???";
+        }
+    }
+    
+    private void ChangeBlinkSprite(bool isBlink)
+    {
+        img.sprite = isBlink ? sprites[1] : sprites[0];
+    }
+
+    private void OnEnable()
+    {
+        PlayerBlinkSystem.OnBlinkChange.AddListener(ChangeBlinkSprite);
+    }
+
+    private void OnDisable()
+    {
+        PlayerBlinkSystem.OnBlinkChange.RemoveListener(ChangeBlinkSprite);
+    }
+}
