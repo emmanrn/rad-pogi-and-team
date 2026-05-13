972734186c74825c6cda1faae9717283b3dcc6f8
RADIAN NAVARRO <radian_navarro@dlsl.edu.ph>
2026-05-13 10:13:14 +0800

    feat: bed added

--- GameJam/Assets/Scripts/SystemComponents/LoadSceneSystem.cs
+++ GameJam/Assets/Scripts/SystemComponents/LoadSceneSystem.cs
@@ -13,11 +13,11 @@ public class LoadSceneSystem : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
-        }  
+        }
         else
             Destroy(gameObject);
     }
-    
+
     public void LoadNextRoom(string currentRoom)
     {
         if (currentRoom == RoomType.Start)
@@ -27,34 +27,35 @@ public class LoadSceneSystem : MonoBehaviour
         else if (currentRoom == RoomType.LivingRoom)
             StartCoroutine(LoadScene(RoomType.PlayRoom));
         else if (currentRoom == RoomType.PlayRoom)
-           StartCoroutine(LoadScene(RoomType.Ending));
+            StartCoroutine(LoadScene(RoomType.Ending));
         else if (currentRoom == RoomType.Ending)
             StartCoroutine(LoadScene(RoomType.Start));
-        
+
     }
-    
+
     //basic loadingsystem. can turn into async operation with loading screen
     //Chat im gonna be honest i dont know how this function works anymore. it jusrt does. dont do anything to it.
     public IEnumerator LoadScene(string sceneName)
-    { 
-       int i = SceneUtility.GetBuildIndexByScenePath(sceneName);
-       if (i <= -1)
-       {
-               Debug.Log("Empty Scene");
-               yield break;
-       }
-       
-       //If player exists, set player to Loading
-       if (Player.Instance != null) 
-           Player.Instance.SetState(Player.State.IsLoading);
-       
-       //Load Scene and wait
-       SceneManager.LoadScene(sceneName);
-       yield return new WaitUntil(() => SceneManager.GetActiveScene().isLoaded);
-       
-       //Reinitialize Player
-       if (Player.Instance != null)
-           Player.Instance.InitializePlayerController();
+    {
+        int i = SceneUtility.GetBuildIndexByScenePath(sceneName);
+        if (i <= -1)
+        {
+            Debug.Log("Empty Scene");
+            yield break;
+        }
+
+        //If player exists, set player to Loading
+        if (Player.Instance != null)
+            Player.Instance.SetState(Player.State.IsLoading);
+
+        //Load Scene and wait
+        SceneManager.LoadScene(sceneName);
+        yield return new WaitUntil(() => SceneManager.GetActiveScene().isLoaded);
+
+
+        //Reinitialize Player
+        if (Player.Instance != null)
+            Player.Instance.InitializePlayerController();
     }
 }
 
@@ -65,5 +66,5 @@ public static class RoomType
     public const string LivingRoom = "LivingRoomScene";
     public const string PlayRoom = "PlayRoomScene";
     public const string Ending = "EndingScene";
-    
+
 }
